74aee1b2026cdf2452332e256d00dc986295e559
Mats Ekelund <mats.ekelund@fro.se>
2023-12-27 16:44:18 +0100

    Trees (3:15:49)

--- js/world.js
+++ js/world.js
@@ -16,8 +16,31 @@ class World {
         this.envelopes = [];
         this.roadBorders = [];
         this.buildings = [];
+        this.trees = [];
 
         this.generate();
+    }
+
+    #generateTrees(count = 10) {
+        const points = [
+            ...this.roadBorders.map((s) => [s.p1, s.p2]).flat(),
+            ...this.buildings.map((b) => b.points).flat()
+        ];
+        const left = Math.min(...points.map((p) => p.x));
+        const right = Math.max(...points.map((p) => p.x));
+        const top = Math.min(...points.map((p) => p.y));
+        const bottom = Math.max(...points.map((p) => p.y));
+
+        const trees = [];
+        while (trees.length < count) {
+
+            const p = new Point(
+                lerp(left, right, Math.random()),
+                lerp(bottom, top, Math.random())
+            );
+            trees.push(p);
+        }
+        return trees;
     }
 
     #generateBuildings() {
@@ -90,6 +113,7 @@ class World {
 
         this.roadBorders = Polygon.union(this.envelopes.map((e) => e.poly));
         this.buildings = this.#generateBuildings();
+        this.trees = this.#generateTrees();
     }
 
     draw(ctx) {
@@ -101,6 +125,9 @@ class World {
         }
         for (const seg of this.roadBorders) {
             seg.draw(ctx, { color: "white", width: 4 });
+        }
+        for (const tree of this.trees) {
+            tree.draw(ctx);
         }
         for (const bld of this.buildings) {
             bld.draw(ctx);
